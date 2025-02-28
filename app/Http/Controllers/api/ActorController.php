16a3a75825d3be9aca7d3fa820ecc669e0127e07
WilbertACDX <115293547+WilbertACDX@users.noreply.github.com>
2025-02-28 19:51:03 +1300

    Actors get
    No se ha probado el método

---
+++ app/Http/Controllers/api/ActorController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Actor;
+use Illuminate\Http\Request;
+
+class ActorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $actors = Actor::all();
+        if(!$actors){
+            return response()->json([
+                'result' => false,
+                'msg' => "No se encontraron actores."
+            ], 404);
+        }
+        return response()->json([
+            'result' => true,
+            'msg' => "Se encontraro los actores.",
+            'data' => $actors
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
